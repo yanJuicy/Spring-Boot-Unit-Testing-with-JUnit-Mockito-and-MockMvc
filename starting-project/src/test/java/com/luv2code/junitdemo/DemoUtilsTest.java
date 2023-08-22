8411e81adf769f985163dcf7a95f511fe2e68065
yanJuicy <hanjub3@gmail.com>
2023-08-23 03:43:37 +0900

    Test Case Lifecycle Methods

--- starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
+++ starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
@@ -1,23 +1,43 @@
 package com.luv2code.junitdemo;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DemoUtilsTest {
 
+    DemoUtils demoUtils;
+
+    @BeforeAll
+    static void setupBeforeAll() {
+        System.out.println("@BeforeAll executes only once before all test methods execution in the class");
+    }
+
+    @AfterAll
+    static void tearDownAfterAll() {
+        System.out.println("@AfterAll executes only once after all test methods execution in the class");
+    }
+
+    @BeforeEach
+    void setupBeforeEach() {
+        demoUtils = new DemoUtils();
+        System.out.println("@BeforeEach executes before the execution of each test method");
+    }
+
+    @AfterEach
+    void tearDownAfterEach() {
+        System.out.println("Running @AfterEach");
+        System.out.println();
+    }
+
     @Test
     void testEqualsAndNotEquals() {
-        DemoUtils demoUtils = new DemoUtils();
-
         assertEquals(6, demoUtils.add(2, 4), "2 + 4 must be 6");
         assertNotEquals(6, demoUtils.add(1, 9), "1 + 9 must not be 6");
     }
 
     @Test
     void testNullAndNotNull() {
-        DemoUtils demoUtils = new DemoUtils();
-
         String str1 = null;
         String str2 = "luv2code";
 
