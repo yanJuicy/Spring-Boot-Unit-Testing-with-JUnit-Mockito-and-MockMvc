35b098f0bafd6fe8f608f1ea1d49d3c3ac70464d
yanJuicy <hanjub3@gmail.com>
2023-08-23 04:09:47 +0900

    Defining Custom Display Names

--- starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
+++ starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class DemoUtilsTest {
 
     DemoUtils demoUtils;
@@ -31,6 +32,7 @@ class DemoUtilsTest {
     }
 
     @Test
+    @DisplayName("Equals and Not Equals")
     void testEqualsAndNotEquals() {
         assertEquals(6, demoUtils.add(2, 4), "2 + 4 must be 6");
         assertNotEquals(6, demoUtils.add(1, 9), "1 + 9 must not be 6");
